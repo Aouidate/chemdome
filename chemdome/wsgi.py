41ebb2102c3e120a9f7770db9898409d61a2f991
Adnane Aouidate <adnane.aouidate@gmail.com>
2024-08-17 17:11:12 +0100

    change app name

--- chemdome/wsgi.py
+++ chemdome/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for domain_app project.
+WSGI config for chemdome project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'domain_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chemdome.settings')
 
 application = get_wsgi_application()
